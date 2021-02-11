f61d0cfe4bfdf2087fab63ee4b3727234d1eea6e
jamalAllal66 <78419147+jamalAllal66@users.noreply.github.com>
2021-02-11 23:36:35 +0100

    Update and rename Blink to blinkAllPins.py

--- blinkAllPins.py
+++ blinkAllPins.py
@@ -7,13 +7,7 @@ pfr = pifacerelayplus.PiFaceRelayPlus(pifacerelayplus.RELAY)
 pfr.init_board({'value': 0, 'direction': 0, 'pullup': 0}, {'value': 0, 'direction': 0, 'pullup': 0})
 
 while True:
-    print ("Off")
-    # set_high will set the pin to voltage 0v
-    pfr.x_pins[0].set_high()
-    pfr.x_pins[1].set_high()
-    pfr.x_pins[2].set_high()
-    pfr.x_pins[3].set_high()
-    time.sleep(2)
+   
     print ("On")
     # set_low will set the pin to voltage (nominally) 3.3v
     pfr.x_pins[0].set_low()
@@ -21,3 +15,12 @@ while True:
     pfr.x_pins[2].set_low()
     pfr.x_pins[3].set_low()
     time.sleep(2)
+    
+    print ("Off")
+    # set_high will set the pin to voltage 0v
+    pfr.x_pins[0].set_high()
+    pfr.x_pins[1].set_high()
+    pfr.x_pins[2].set_high()
+    pfr.x_pins[3].set_high()
+    time.sleep(2)
+    
